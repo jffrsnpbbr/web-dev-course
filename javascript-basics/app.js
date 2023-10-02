c0415c6eed9bc99ceacceb527104c98cdaa772a2
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-02 11:50:14 +0800

    Onwards to custom commands

--- javascript-basics/app.js
+++ javascript-basics/app.js
@@ -2,12 +2,15 @@ let age = 32;
 let userName = 'Max';
 let hobbies = ['Sports', 'Cooking', 'Reading'];
 let job = {
-    title: 'Developer',
-    place: 'New York',
-    salary: 50000
+  title: 'Developer',
+  place: 'New York',
+  salary: 50000,
 };
 
-age = 45;
-
 let adultYears = age - 18;
-alert(adultYears)
+alert(adultYears);
+
+age = 45;
+adultYears = age - 18;
+
+alert(adultYears);
