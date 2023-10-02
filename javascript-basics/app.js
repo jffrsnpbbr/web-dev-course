683dc44651eb3a6b29466987c617d0f11f3a1887
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-02 11:02:37 +0800

    Introducing Objects

--- javascript-basics/app.js
+++ javascript-basics/app.js
@@ -1,5 +1,5 @@
 let age = 32;
 let userName = 'Max';
 let hobbies = ['Sports', 'Cooking', 'Reading'];
-
- alert(hobbies[0]);
+let job = { title: 'Developer', place: 'New York', salary: 50000 };
+alert(hobbies[0]);
