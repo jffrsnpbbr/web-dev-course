beda612cfa044f70385ee9a6c4c91881517da0cc
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-02 11:28:46 +0800

    Splitting Code Across Multiple Lines

--- javascript-basics/app.js
+++ javascript-basics/app.js
@@ -1,5 +1,10 @@
 let age = 32;
-let userName = 'Max';
+let userName = 'Max' + 'Schwaz';
 let hobbies = ['Sports', 'Cooking', 'Reading'];
-let job = { title: 'Developer', place: 'New York', salary: 50000 };
+let job = {
+    title: 'Developer',
+    place: 'New York',
+    salary: 50000
+};
+
 alert(hobbies[0]);
