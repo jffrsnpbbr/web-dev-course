5b177154b309f8f06ba1304f9b2cbb466e2bfb1b
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-02 16:00:52 +0800

    Basic Array Operations

--- javascript-basics/operations.js
+++ javascript-basics/operations.js
@@ -32,3 +32,6 @@ console.log('Max' - 'ax');
 let userName = 'Max';
 console.log(userName.length);
 console.log(userName.toUpperCase());
+
+let hobbies = ['Sports', 'Cooking'];
+console.log(hobbies.length);
