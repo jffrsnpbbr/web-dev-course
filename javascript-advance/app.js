f264ef15dc9a6826dfe078921306c96bdd8c3edf
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-05 17:06:45 +0800

    Searching The DOM For Elements

--- javascript-advance/app.js
+++ javascript-advance/app.js
@@ -1,4 +1,9 @@
-document.body.children[1].children[0].href='https://google.com';
+// document.body.children[1].children[0].href='https://google.com';
 
+
+// console.dir(document);
 // alert();
 // window.alert();
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com'
