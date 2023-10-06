ea0c0c5e3328b16c786f3729cfaf3ac650c4948b
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-06 16:48:18 +0800

    Introducing Constant Variables

--- javascript-advance/app.js
+++ javascript-advance/app.js
@@ -1,21 +1,31 @@
-let paragraphElement = document.querySelector('p');
+// let paragraphElement = document.querySelector('p');
 
-function changeParagraphText(event) {
-  paragraphElement.textContent = 'Clicked!';
-  console.log('Paragraph clicked!');
-  console.log(event);
-}
+// function changeParagraphText(event) {
+//   paragraphElement.textContent = 'Clicked!';
+//   console.log('Paragraph clicked!');
+//   console.log(event);
+// }
 
-paragraphElement.addEventListener('click', changeParagraphText);
+// paragraphElement.addEventListener('click', changeParagraphText);
 
-let inputElement = document.querySelector('input');
+// let inputElement = document.querySelector('input');
 
-function retrieveUserInput(event) {
-  // let enteredText = inputElement.value;
-  let enteredText = event.target.value;
-  // let enteredText = event.data;
-  console.log(enteredText);
-  console.log(event);
-}
+// function retrieveUserInput(event) {
+//   // let enteredText = inputElement.value;
+//   let enteredText = event.target.value;
+//   // let enteredText = event.data;
+//   console.log(enteredText);
+//   console.log(event);
+// }
 
-inputElement.addEventListener('input', retrieveUserInput);
+// inputElement.addEventListener('input', retrieveUserInput);
+
+
+// Introducing Constant Variables
+
+const userName = 'Max';
+let age = 32;
+
+// executing some code ...
+
+age = 33;
