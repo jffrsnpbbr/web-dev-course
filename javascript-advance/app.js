9df134763b25b8e66bd719be3ee4e4b6c3e5f9fa
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-06 08:48:32 +0800

    Moving Existing Elements Around

--- javascript-advance/app.js
+++ javascript-advance/app.js
@@ -28,8 +28,14 @@ firstParagraph.append(newAnchorElement);
 // REMOVE ELEMENTS
 
 // 1. Select the element that should be removed
-// 2. Remove it
 
 let firstH1Element = document.querySelector('h1');
+
+// 2. Remove it
+
 // firstH1Element.remove();
 firstH1Element.parentElement.removeChild(firstH1Element); // for older browsers;
+
+// MOVE ELEMENTS
+
+firstParagraph.parentElement.append(firstParagraph);
