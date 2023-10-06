ab45d53f21b6e88cd2364d8d33ea6f5dae9d118d
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-06 19:55:35 +0800

    Time to Practice: The Solution

--- javascript-advance/demo.js
+++ javascript-advance/demo.js
@@ -5,9 +5,17 @@ const maxAllowedChars = productNameInputElement.maxLength;
 function updateRemainingCharacters(event) {
   const enteredText = event.target.value;
   const enteredTextLength = enteredText.length
-  
+
   const remainingCharacters = maxAllowedChars - enteredTextLength; 
   remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters == 0) {
+    productNameInputElement.classList.add('warning');
+    remainingCharsElement.classList.add('warning');
+  } else {
+    productNameInputElement.classList.remove('warning');
+    remainingCharsElement.classList.remove('warning');
+  }
 }
 
 productNameInputElement.addEventListener('input', updateRemainingCharacters);
