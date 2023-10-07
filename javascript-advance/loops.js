51b22cd882decc1bfeee247895d9072e4e24dd80
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-07 16:36:17 +0800

    The while loop

--- javascript-advance/loops.js
+++ javascript-advance/loops.js
@@ -18,3 +18,10 @@ for (const propertyName in loggedInUser) {
   console.log(propertyName);
   console.log(loggedInUser[propertyName]);
 }
+
+let isFinished = false;
+
+while (!isFinished) {
+  isFinished = confirm('Do you want to quit?');
+
+}
