ae8ac49808eb3bd9b1f6723b1384449dc406cc23
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-12 18:38:06 +0800

    Storing and Managing Submitted Data + Adding Logic For Starting a Game

--- miles-stone-project/scripts/config.js
+++ miles-stone-project/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editPlayer = +event.target.dataset.playerid;
   playerConfigOverlayElement.style.display = 'block';
   backdropElement.style.display = 'block';
 }
@@ -6,4 +7,27 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
+  formElement.firstElementChild.lastElementChild.value = ''
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get('playername').trim();
+  console.log(enteredPlayerName);
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please input a valid name!';
+
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById('player-' + editPlayer + '-data');
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editPlayer-1].name = enteredPlayerName;
+  
+  closePlayerConfig();
 }
