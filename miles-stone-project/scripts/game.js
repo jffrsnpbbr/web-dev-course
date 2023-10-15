581a1039194eac5335032590b94a69bfc9b610b0
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-15 15:21:41 +0800

    Ending The Game & Adding Restart Logic

--- miles-stone-project/scripts/game.js
+++ miles-stone-project/scripts/game.js
@@ -1,8 +1,30 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstChild.innerHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span>';
+  gameOverElement.style.display = 'none';
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (players[0].name === '' || players[1].name === '') {
     alert('Please set custom player names for both players!');
     return;
   }
+
+  resetGameStatus();
 
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = 'block';
@@ -50,11 +72,10 @@ function checkForGameOver() {
     return gameData[0][0];
   }
 
-
   if (currentRound === 9) {
     return -1;
   }
-  
+
   return 0;
 }
 
@@ -66,7 +87,7 @@ function switchPlayer() {
 function selectGameField(event) {
   const selectedField = event.target;
 
-  if (selectedField.tagName !== 'LI') {
+  if (selectedField.tagName !== 'LI' || gameIsOver) {
     return;
   }
 
@@ -84,8 +105,23 @@ function selectGameField(event) {
   gameData[selectedRow][selectedCol] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
-  
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  } 
+
   currentRound++;
   switchPlayer();
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = 'block';
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw!";
+  }
 }
