64a056762774c94ea2bbc4535ddebca20242a8ec
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-16 11:41:38 +0800

    Reading File Data & Returning Dynamic Responses

--- nodejs-introduction/app.js
+++ nodejs-introduction/app.js
@@ -7,31 +7,44 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/currenttime', function(req, res) {
+app.get('/currenttime', function (req, res) {
   res.send('<h1>' + new Date().toISOString() + '</h1>');
 }); // localhost:3000/currenttime
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send(
     '<form action="/store-user" method="post"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>'
   );
 }); // localhost:3000
 
-app.post('/store-user', function(req, res) {
+app.post('/store-user', function (req, res) {
   const username = req.body.username;
 
   const filePath = path.join(__dirname, 'data', 'users.json');
-  
+
   const fileData = fs.readFileSync(filePath);
   const existingUsers = JSON.parse(fileData);
-  
-  existingUsers.push(username);;
+
+  existingUsers.push(username);
 
   fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
-  res.send(
-    '<h1>Username stored!</h1>'
-  );
-}); // localhost:3000
+  res.send('<h1>Username stored!</h1>');
+});
+
+app.get('/users', function (req, res) {
+  const filePath = path.join(__dirname, 'data', 'users.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = '<ul>';
+
+  for (const user of existingUsers) {
+    responseData += '<li>' + user + '</li>';
+  }
+  responseData += '</ul>';
+  res.send(responseData);
+});
 
 app.listen(3000);
