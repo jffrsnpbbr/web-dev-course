281117ea9f52454b5853ca19d7aa634a438a5fe7
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-16 21:59:51 +0800

    Serving Static FIles (CSS & JavaScript)

--- more-express-satic-dynamic-content/app.js
+++ more-express-satic-dynamic-content/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'index.html');
