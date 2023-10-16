6952ab3630ea011d5b5602a17fa64e7489f45ce9
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-16 11:19:08 +0800

    parsing User Data With Express

--- nodejs-introduction/app.js
+++ nodejs-introduction/app.js
@@ -1,7 +1,10 @@
+const { json } = require('body-parser');
 const express = require('express');
 const { resolve } = require('path/win32');
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/currenttime', function(req, res) {
   res.send('<h1>' + new Date().toISOString() + '</h1>');
@@ -9,8 +12,16 @@ app.get('/currenttime', function(req, res) {
 
 app.get('/', function(req, res) {
   res.send(
-    '<h1>Hello World!</h1>' + 
-    '<a href="currenttime">Current Time</a>'
+    '<form action="/store-user" method="post"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>'
+  );
+}); // localhost:3000
+
+app.post('/store-user', function(req, res) {
+  const username = req.body.username;
+  console.log(username);
+
+  res.send(
+    '<h1>Username stored!</h1>'
   );
 }); // localhost:3000
 
