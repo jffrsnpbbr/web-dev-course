9f0c078fe4f941e8d1dfecd0605788d480d8977b
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-16 08:41:42 +0800

    Doing More Server-side Work

--- nodejs-introduction/app.js
+++ nodejs-introduction/app.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
 function handleRequest(request, response) {
-  response.statusCode = 200;
-  response.end('<h1>Hello World!</h1>');
+  if (request.url === '/currenttime') {
+    response.statusCode = 200;
+    response.end('<h1>' + new Date().toISOString() + '</h1>');
+  } else if (request.url === '/') {
+    response.statusCode = 200;
+    response.end('<h1>Hello World!</h1>');
+  }
+
+  response.statusCode = 404;
+  response.end('<h1>Page not found!</h1>');
 }
 
 const server = http.createServer(handleRequest);
