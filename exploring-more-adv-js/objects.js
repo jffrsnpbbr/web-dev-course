3e41d6e432a12cec6220f9ba7817ec1681ddb4a9
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-18 11:03:09 +0800

    Classes and Methods and this

--- exploring-more-adv-js/objects.js
+++ exploring-more-adv-js/objects.js
@@ -1,13 +1,12 @@
-
 // Object literal
-const job = {
-  title: 'Developer',
-  location: 'New York',
-  salary: 50000
-}
+// const job = {
+//   title: 'Developer',
+//   location: 'New York',
+//   salary: 50000
+// }
 
-const date = new Date().toISOString();
-console.log(date);
+// const date = new Date().toISOString();
+// console.log(date);
 
 class Job {
   constructor(title, location, salary) {
@@ -15,7 +14,16 @@ class Job {
     this.location = location;
     this.salary = salary;
   }
+
+  describe() {
+    console.log(
+      `I'm a ${this.title}, I work in ${this.location} and I earn ${this.salary}.`
+    );
+  }
 }
 
-const developer = new Job('Developer', 'Philippines', 120000)
-console.log(developer);
+const developer = new Job('Developer', 'Philippines', 120000);
+const cook = new Job('Cook', 'Manila', 35000);
+
+developer.describe();
+cook.describe();
