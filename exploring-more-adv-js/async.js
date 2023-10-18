6c22e32ab8826e3c5a8d092c1c6ec532b3712574
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-18 12:14:26 +0800

    Improving Code with async / await

--- exploring-more-adv-js/async.js
+++ exploring-more-adv-js/async.js
@@ -1,25 +1,39 @@
 // const fs = require('fs');
 const fs = require('fs/promises');
 
-function readFile() {
+// function readFile() {
+//   let fileData;
+
+//   // fs.readFile('data.txt', function(error, fileData) {
+//   //   console.log('File parsing done!');
+//   //   console.log(fileData.toString());
+//   //   //  start another async task that sends the data to a database
+//   // });
+
+//   fs.readFile('data.txt')
+//     .then(function(dataFile) {
+//       console.log(dataFile.toString());
+//       // return anotherAsyncOperation;
+//     })
+//     .then(function() {
+//     })
+//     .catch(function(error){
+//       console.log(error.message);
+//     });
+
+//   console.log('Hi there!');
+// }
+
+async function readFile() {
   let fileData;
 
-  // fs.readFile('data.txt', function(error, fileData) {
-  //   console.log('File parsing done!');
-  //   console.log(fileData.toString());
-  //   //  start another async task that sends the data to a database
-  // });
+  try {
 
-  fs.readFile('data.txt')
-    .then(function(dataFile) {
-      console.log(dataFile.toString());
-      // return anotherAsyncOperation;
-    })
-    .then(function() {
-    })
-    .catch(function(error){
-      console.log(error.message);
-    });
+    fileData = await fs.readFile('data.txt')
+    console.log(fileData.toString());
+  } catch (error) {
+    console.log(error.message);
+  }
 
   console.log('Hi there!');
 }
