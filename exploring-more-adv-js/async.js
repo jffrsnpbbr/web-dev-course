64d2b8efe724d4818b1781be1029a5227edd353b
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-18 12:02:28 +0800

    Introducing Promises

--- exploring-more-adv-js/async.js
+++ exploring-more-adv-js/async.js
@@ -1,13 +1,22 @@
-const fs = require('fs');
+// const fs = require('fs');
+const fs = require('fs/promises');
 
 function readFile() {
   let fileData;
 
-  fs.readFile('data.txt', function(error, fileData) {
-    console.log('File parsing done!');
-    console.log(fileData.toString());
+  // fs.readFile('data.txt', function(error, fileData) {
+  //   console.log('File parsing done!');
+  //   console.log(fileData.toString());
+  //   //  start another async task that sends the data to a database
+  // });
 
-  });
+  fs.readFile('data.txt')
+    .then(function(dataFile) {
+      console.log(dataFile.toString());
+      // return anotherAsyncOperation;
+    })
+    .then(function() {
+    });
 
   console.log('Hi there!');
 }
