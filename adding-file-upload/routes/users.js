e3ddc02c3e9eea80ef370c1724d8d85932ce1498
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-22 16:47:57 +0800

    Configuring Multer in-depth

--- adding-file-upload/routes/users.js
+++ adding-file-upload/routes/users.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const multer = require('multer');
 
-const upload = multer({ dest: 'images/' });
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
 const db = require('../data/database');
@@ -19,6 +28,9 @@ router.post('/profiles', upload.single('image'), function (req, res) {
   const userData = req.body;
 
   console.log(uploadedImageFile);
+  console.log(userData);
+  
+  res.redirect('/');
 });
 
 module.exports = router;
