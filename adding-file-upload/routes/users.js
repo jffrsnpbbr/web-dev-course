c36f8c2b2eb687ee3e9a09bf615399f57e5ab5a1
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-22 17:01:28 +0800

    Serving Uploaded Files To Website Visitors

--- adding-file-upload/routes/users.js
+++ adding-file-upload/routes/users.js
@@ -15,21 +15,22 @@ const router = express.Router();
 
 const db = require('../data/database');
 
-router.get('/', function (req, res) {
-  res.render('profiles');
+router.get('/', async function (req, res) {
+  const users = await db.getDb().collection('users').find().toArray();
+  res.render('profiles', { users });
 });
 
 router.get('/new-user', function (req, res) {
   res.render('new-user');
 });
 
-router.post('/profiles', upload.single('image'), async  function (req, res) {
+router.post('/profiles', upload.single('image'), async function (req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
- 
+
   await db.getDb().collection('users').insertOne({
     name: userData.username,
-    imagePath: uploadedImageFile.path
+    imagePath: uploadedImageFile.path,
   });
 
   res.redirect('/');
