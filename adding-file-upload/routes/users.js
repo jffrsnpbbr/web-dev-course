f21920769439432132cf7b914506397dde240446
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-22 15:47:46 +0800

    How To Store File On A Backend

--- adding-file-upload/routes/users.js
+++ adding-file-upload/routes/users.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const multer = require('multer');
 
-const upload = multer({})
+const upload = multer({ dest: 'images/' });
 const router = express.Router();
-router.get('/', function(req, res) {
+
+const db = require('../data/database');
+
+router.get('/', function (req, res) {
   res.render('profiles');
 });
 
-router.get('/new-user', function(req, res) {
+router.get('/new-user', function (req, res) {
   res.render('new-user');
 });
 
-router.post('/profiles', upload.single('image') , function (req, res) {
+router.post('/profiles', upload.single('image'), function (req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
+
+  console.log(uploadedImageFile);
 });
 
 module.exports = router;
