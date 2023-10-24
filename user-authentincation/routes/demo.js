621bcc91f1a89b94330f7b2c034c0fe477ad1c1c
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-24 16:26:42 +0800

    Adding a Basic Signup Functionality

--- user-authentincation/routes/demo.js
+++ user-authentincation/routes/demo.js
@@ -12,6 +12,22 @@ router.get('/signup', function (req, res) {
   res.render('signup');
 });
 
+router.post('/signup', async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData['confirm-email'];
+  const enteredPassword = userData['password'];
+
+  const user = {
+    email: enteredEmail,
+    password: enteredPassword,
+  };
+
+  await db.getDb().collection('users').insertOne(user);
+
+  res.redirect('/login');
+});
+
 router.get('/login', function (req, res) {
   res.render('login');
 });
