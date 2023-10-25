c79ef5369d30e3507610dccbfc3ae4d1fc709e72
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-26 03:35:29 +0800

    Diving Deeper into Sessions (Beyond Authentication)

--- user-authentincation/routes/demo.js
+++ user-authentincation/routes/demo.js
@@ -11,7 +11,19 @@ router.get('/', function (req, res) {
 });
 
 router.get('/signup', function (req, res) {
-  res.render('signup');
+  let sessionInputData = req.session.inputData;
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: '',
+      confirmEmail: '',
+      password: '',
+    };
+  }
+
+  req.session.inputData = null;
+
+  res.render('signup', { inputData: sessionInputData });
 });
 
 router.get('/login', function (req, res) {
@@ -32,8 +44,19 @@ router.post('/signup', async function (req, res) {
     enteredEmail !== enteredConfirmEmail ||
     !enteredEmail.includes('@')
   ) {
-    console.log('Incorrect Data');
-    return res.redirect('/signup');
+    req.session.inputData = {
+      hasError: true,
+      message: 'Invalid input - please check your data.',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+
+    req.session.save(function () {
+      res.redirect('/signup');
+    });
+
+    return;
   }
   const existingUser = await db
     .getDb()
@@ -106,7 +129,7 @@ router.get('/admin', function (req, res) {
 router.post('/logout', function (req, res) {
   req.session.user = null;
   req.session.isAuthenticated = false;
-  res.redirect('/')
+  res.redirect('/');
 });
 
 module.exports = router;
