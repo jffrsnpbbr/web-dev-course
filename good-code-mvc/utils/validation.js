daa104f9affcea9a484c8469a64e997eda1ba7aa
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-10-31 19:07:57 +0800

    validation userIsValid

--- good-code-mvc/utils/validation.js
+++ good-code-mvc/utils/validation.js
@@ -2,6 +2,17 @@ function postIsValid(title, content) {
   return (title && content && title.trim() !== '') || content.trim() !== '';
 }
 
+function userIsValid(email, confirmEmail, password) {
+  return (
+    email &&
+    confirmEmail &&
+    password &&
+    password.trim().length > 6 &&
+    email === confirmEmail &&
+    confirmEmail.includes('@')
+  );
+}
+
 module.exports = {
   postIsValid,
 };
